af12b53a70ea970a8cd7443e8e677978eb3a83b3
parasbhanot <paras.bhanot@gmail.com>
2016-10-04 00:21:03 +0530

    initial commit
    files added

---
+++ README.md
@@ -0,0 +1,14 @@
+Basic info :
+==============
+
+This project uses MSQL database so in order to use it you will have to install MSQL and modify the user-name and password . 
+
+>**Library used :**
+
+> -  Jssc
+> - Jfree chart 
+> - MySql driver 
+
+
+[Video Demo](https://www.youtube.com/edit?video_id=RzD_UL73Y_k)
+
---
+++ src/simpleSerial/Mainly.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleSerial;
+
+/**
+ *
+ * @author parasbhanot
+ */
+public class Mainly {
+    
+    public static void main(String [] args){
+        
+          try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(subFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(subFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(subFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(subFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new subFrame().setVisible(true);
+            }
+        });
+    }
+        
+    }
+    
+
